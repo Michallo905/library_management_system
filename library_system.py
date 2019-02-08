29bdf66621db4cfd4e100b769d1f38965b999f4d
Krzysztof Czerskip <krzysztof.czerskip@gmail.com>
2019-02-08 22:57:22 +0100

    main function

--- library_system.py
+++ library_system.py
@@ -31,5 +31,58 @@ class Library:
       def addBook(self,returnedBook):
             self.availablebooks.append(returnedBook)
             print("Thanks for returning your borrowed book")
-  
 
+
+
+def main():
+      library=Library(["The Last Battle","The Screwtape letters","The Great Divorce"])
+      student=Student()
+      librarian=Librarian()
+      done=False
+      while done==False:
+            print("""Continue as:
+            1.Librarian
+            2.Student
+                  """)
+            choice=int(input("Enter Choice:"))
+            if choice==2:
+                  print(""" ======LIBRARY MENU=======
+                  1. Display all available books
+                  2. Request a book
+                  3. Return a book
+                  4. Exit
+                       """)
+                  choice=int(input("Enter Choice:"))
+                  if choice==1:
+                        library.displayAvailablebooks()
+                  elif choice==2:
+                        library.lendBook(student.requestBook())
+                  elif choice==3:
+                        library.addBook(student.returnBook())
+                  elif choice==4:
+                        sys.exit()
+            elif choice==1:
+                  print(""" ======LIBRARIAN MENU=======
+                  1. Show all books
+                  2. Search a book
+                  3. Delete a book
+                  4. Request a book
+                  5. Return a book
+                  6. Exit
+                       """)
+                  choice=int(input("Enter Choice:"))
+                  if choice==1:
+                        library.displayAvailablebooks()
+                  elif choice==2:
+                        library.search_Book(librarian.searchBook())
+                  elif choice==3:
+                        library.delete_Book(librarian.deleteBook())
+                  elif choice==4:
+                        library.lendBook(librarian.requestBook())
+                  elif choice==5:
+                        library.addBook(librarian.returnBook())
+                  elif choice==6:
+                        sys.exit()
+
+
+main()
